672906019bdf7ce3bb3af3c875c276cd023afe33
Robert-Jung <rjinjung@gmail.com>
2017-08-05 16:15:54 -0700

    Adding routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import { routes as childRoutes, ProductsModule } from './products/products.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProtectedComponent } from './protected/protected.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+
+import { AUTH_PROVIDERS } from './auth.service';
+import { LoggedInGuard } from './logged-in.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contactus', redirectTo: 'contact' },
+
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ LoggedInGuard ]
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    children: childRoutes
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ProtectedComponent
+    ProtectedComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ProductsModule
   ],
-  providers: [],
+  providers: [
+    AUTH_PROVIDERS,
+    LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
